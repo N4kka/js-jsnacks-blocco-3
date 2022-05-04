32c246d2feb00eefd2cbadbdc6016d68fd7b22ff
N4kka <sarlinicola7@gmail.com>
2022-05-04 16:22:40 +0200

    snack 3 scaffolding

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,1 +1,16 @@
-alert("ciao");
+const totalNumbers = [];
+
+const userNumbers = parseInt(prompt("dimmi un numero"));
+
+for(let i = 0; i < 50; i++) {
+    const items = totalNumbers[i];
+    if(totalNumbers <= 50) {
+        const sumNumbers = userNumbers.reduce(
+            (previousValue, currentValue) => previousValue + currentValue, totalNumbers,
+                totalNumbers.push(sumNumbers)
+        )
+    } else {
+        console.log("niente");
+    }
+};
+console.log(totalNumbers, userNumbers);
