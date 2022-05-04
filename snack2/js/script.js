70fd23dde5fe8cb30b3489f06a61bda5aa345a98
N4kka <sarlinicola7@gmail.com>
2022-05-04 17:43:19 +0200

    having some troubles to sum and push asked numbers into the empty array until 50

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -4,11 +4,9 @@ const userNumbers = parseInt(prompt("dimmi un numero"));
 
 for(let i = 0; i < 50; i++) {
     const items = totalNumbers[i];
-    if(totalNumbers <= 50) {
-        const sumNumbers = userNumbers.reduce(
-            (previousValue, currentValue) => previousValue + currentValue, totalNumbers,
-                totalNumbers.push(sumNumbers)
-        )
+    if(totalNumbers < 50) {
+        const sumNumbers = userNumbers + userNumbers;
+        totalNumbers.push(sumNumbers);
     } else {
         console.log("niente");
     }
